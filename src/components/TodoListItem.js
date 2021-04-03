58076dfb32061f03efab434c2c6d90aab6eee623
suraj009 <surajrajmane18@gmail.com>
2021-04-03 12:18:01 +0530

    check empty string condition

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -18,7 +18,7 @@ const TodoListItem = ({ todo, deleteTodo, startEditing, finishEditing }) => {
 
     const finishEdit = () => {
 
-        if(todoValue.trim()) {
+        if(todoValue.trim() !== "") {
             todo.value = todoValue.trim();
             todo.isEditing = false;
             finishEditing(todo);
@@ -30,7 +30,7 @@ const TodoListItem = ({ todo, deleteTodo, startEditing, finishEditing }) => {
         <li className="list">
             {todo.isEditing ? (
                 <>
-                    <textarea className="editTask" defaultValue={todoValue.trim()} onChange={changeTodoValue}></textarea>
+                    <textarea className="editTask" defaultValue={todoValue} onChange={changeTodoValue}></textarea>
                     <button className="saveTask" onClick={finishEdit}>Save</button>
                 </>
 
