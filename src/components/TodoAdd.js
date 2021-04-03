72ea16349c5f2fd9bcbf35fe8f5658ed69175d04
suraj009 <surajrajmane18@gmail.com>
2021-04-03 11:08:54 +0530

    made textarea in TodoAdd a controlled component

--- src/components/TodoAdd.js
+++ src/components/TodoAdd.js
@@ -1,23 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/TodoAdd.css'
 
 function TodoAdd({addTodo}) {
-    let TodoValue = "";
+    // let TodoValue = "";
+    const [todoValue, setTodoValue] = useState("");
     const submitTodo = () => {
-        if(TodoValue.trim()) {
-            addTodo(TodoValue.trim());
-            TodoValue = "";
-            document.getElementById("task").value = "";
+        // if(TodoValue.trim()) {
+        //     addTodo(TodoValue.trim());
+        //     TodoValue = "";
+        //     document.getElementById("task").value = "";
+        // }
+
+        if(todoValue.trim()) {
+            addTodo(todoValue.trim());
+            setTodoValue("");
         }
     }
 
     const updateTodoValue = e => {
-        TodoValue = e.target.value;
+        // TodoValue = e.target.value;
+        setTodoValue(e.target.value)
         
     }
     return (
         <div className="grid-container">
-            <textarea id="task" onChange={updateTodoValue}></textarea>
+            <textarea id="task" onChange={updateTodoValue} value={todoValue}></textarea>
             <br/>
             <button id="btn" onClick={submitTodo}>Add New</button>
         </div>
